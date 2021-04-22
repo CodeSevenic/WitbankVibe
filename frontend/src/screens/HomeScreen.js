5f2c16901c8b03964463d322ef72b267c241545d
CodeSevenic <sbu.audio.production@gmail.com>
2021-04-22 13:32:04 +0200

    my changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,23 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Products from '../components/Products';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function HomeScreen() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
+      try {
+        setLoading(true);
+        const { data } = await axios.get('/api/products');
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
     };
-
     fetchData();
   }, []);
 
   return (
-    <div className="row center">
-      {products.map((product) => (
-        <Products key={product._id} product={product}></Products>
-      ))}
-    </div>
+    <React.Fragment>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Products key={product._id} product={product}></Products>
+          ))}
+        </div>
+      )}
+    </React.Fragment>
   );
 }
