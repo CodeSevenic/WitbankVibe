8dd875f66b48c249086eed3088baf5d9ed926be4
CodeSevenic <sbu.audio.production@gmail.com>
2021-04-22 20:27:08 +0200

    my changes

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,69 +1,79 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+
 import Rating from '../components/Ratings';
-import data from '../data';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 export default function ProductScreen(props) {
-  const product = data.products.find((x) => x._id === props.match.params.id);
-  if (!product) {
-    return <div>Product Not Found</div>;
-  }
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
   return (
-    <React.Fragment>
-      <Link to="/">Back to results</Link>
-      <div className="product-contents">
-        <div className="product_img">
-          <img className="" src={product.image} alt={product.name} />
-        </div>
-        <div className="product-details-wrap">
-          <div className="product-details">
-            <ul>
-              <li>
-                <h1>{product.name}</h1>
-              </li>
-              <li>
-                <Rating
-                  rating={product.rating}
-                  numReviews={product.numReviews}
-                />
-              </li>
-              <li>Price: R {product.price}</li>
-              <li>
-                Description: <p>{product.description}</p>
-              </li>
-            </ul>
-          </div>
-          <div className="product-details">
-            <div className="product-details-card card-body">
-              <ul>
-                <li>
-                  <div className="row">
-                    <div>Price:</div>
-                    <div className="price">R {product.price}</div>
-                  </div>
-                </li>
+    <div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <React.Fragment>
+          <Link to="/">Back to results</Link>
+          <div className="product-contents">
+            <div className="product_img">
+              <img className="" src={product.image} alt={product.name} />
+            </div>
+            <div className="product-details-wrap">
+              <div className="product-details">
+                <ul>
+                  <li>
+                    <h1>{product.name}</h1>
+                  </li>
+                  <li>
+                    <Rating
+                      rating={product.rating}
+                      numReviews={product.numReviews}
+                    />
+                  </li>
+                  <li>Price: R {product.price}</li>
+                  <li>
+                    Description: <p>{product.description}</p>
+                  </li>
+                </ul>
+              </div>
+              <div className="product-details">
+                <div className="product-details-card card-body">
+                  <ul>
+                    <li>
+                      <div className="row">
+                        <div>Price:</div>
+                        <div className="price">R {product.price}</div>
+                      </div>
+                    </li>
 
-                <li>
-                  <div className="row">
-                    <div>Status:</div>
-                    <div>
-                      {product.countInStock > 0 ? (
-                        <span className="success">In Stock</span>
-                      ) : (
-                        <span className="danger">Not Available</span>
-                      )}
-                    </div>
-                  </div>
-                </li>
+                    <li>
+                      <div className="row">
+                        <div>Status:</div>
+                        <div>
+                          {product.countInStock > 0 ? (
+                            <span className="success">In Stock</span>
+                          ) : (
+                            <span className="danger">Not Available</span>
+                          )}
+                        </div>
+                      </div>
+                    </li>
 
-                <li>
-                  <button className="primary block">Add to Cart</button>
-                </li>
-              </ul>
+                    <li>
+                      <button className="primary block">Add to Cart</button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </React.Fragment>
+        </React.Fragment>
+      )}
+    </div>
   );
 }
